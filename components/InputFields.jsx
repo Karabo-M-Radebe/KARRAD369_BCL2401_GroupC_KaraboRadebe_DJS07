9e05d74ebdfdc034e785874f2bb0c3d9fc5a0156
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-05-30 09:49:24 +0200

    Refactor InputFields component to use an object state for meme data and update getMemeImage function to update state accordingly.

--- components/InputFields.jsx
+++ components/InputFields.jsx
@@ -3,13 +3,22 @@ import memesData from "./memesData";
 
 export default function InputFields () {
 
-    const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg",
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
     
     const getMemeImage = () => {
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        url = memesArray[randomNumber].url
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme, 
+            randomImage: url
+        }))
     }
 
     return(
@@ -26,7 +35,7 @@ export default function InputFields () {
                 
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
             </form>
-            <img src={memeImage} className="meme--image"/>
+            <img src={meme.randomImage} className="meme--image"/>
         </main>
 
     )
